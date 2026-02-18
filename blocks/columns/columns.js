88dd78e66706e414fb16aa99e01e2a36838a0efc
Fran López <confrlop@nibeconsultants.eu>
2026-02-18 13:41:45 +0100

    Changes on Grid (Columns block)

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -3,58 +3,49 @@ export default function decorate(block) {
   block.classList.add(`columns-${cols.length}-cols`);
   const blockClasses = block.getAttribute('class');
 
-  // const wrapper = block.firstElementChild;
-  // wrapper.setAttribute('class', 'columns-row');
-
-  // if (blockClasses.includes('onethird')) {
-  //   const element1 = block.firstElementChild.children[0];
-  //   element1.setAttribute('class','col-third');
-  //   const element2 = block.firstElementChild.children[1];
-  //   element2.setAttribute('class','col-two-thirds');
-  // }
-
-  // setup image columns
+  // Setup columns based on Grid
   [...block.children].forEach((row) => {
     row.setAttribute('class', 'columns-row');
-    console.log("row",row);
     [...row.children].forEach((col, index) => {
-      // console.log("length",[...row.children].length);
-      if ([...row.children].length === 3) {
-        col.setAttribute('class', 'col-third');
+      if (cols.length === 3) {
+        col.setAttribute('class', 'col-33');
       } else {
-        console.log('Columns');
-        if (blockClasses.includes('onethird')) {
+        const textElement = col.querySelector('p');
+        const picElement = row.querySelector('picture');
+
+        if (blockClasses.includes('col-33-66')) {
           if (index === 0) {
-            col.setAttribute('class', 'col-third');
+            col.setAttribute('class', 'col-33');
           } else {
-            const text = col.querySelector('p');
-            if (text) {
-              col.classList.add('col-two-thirds-right-txt');
+            if (textElement && picElement) {
+              const textWrapper = textElement.closest('div');
+              textWrapper.classList.add('col-66-right-txt');
             }
-            col.classList.add('col-two-thirds');
+            col.classList.add('col-66');
           }
-        } else if (blockClasses.includes('twothirds')) {
+        } else if (blockClasses.includes('col-66-33')) {
           if (index === 0) {
-            const text = col.querySelector('p');
-            if (text) {
-              col.classList.add('col-two-thirds-left-txt');
+            if (textElement && picElement) {
+              const textWrapper = textElement.closest('div');
+              textWrapper.classList.add('col-66-left-txt');
             }
-            col.classList.add('col-two-thirds');
+            col.classList.add('col-66');
           } else {
-            col.setAttribute('class', 'col-third col-start-two-thirds');
+            col.classList.add('col-33', 'col-start-two-thirds');
           }
         } else {
-          col.setAttribute('class', 'col-half');
+          col.classList.add('col-half');
+          if (textElement && picElement) {
+            const textWrapper = textElement.closest('div');
+            if (index === 0) {
+              textWrapper.classList.add('col-half-left-txt');
+              col.nextElementSibling.classList.add('col-start-half');
+            } else {
+              textWrapper.classList.add('col-half-right-txt');
+            }
+          }
         }
       }
-      // const pic = col.querySelector('picture');
-      // if (pic) {
-      //   const picWrapper = pic.closest('div');
-      //   if (picWrapper && picWrapper.children.length === 1) {
-      //     // picture is only content in column
-      //     picWrapper.classList.add('columns-img-col');
-      //   }
-      // }
     });
   });
 }
